0b8ce93c98ef764e2a46c6095e6754ac608c02c0
mahdi <falahati.mahdi@yahoo.com>
2022-10-14 17:26:48 +0330

    add: home view

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import router from './router'
 Vue.config.productionTip = false
+export const bus = new Vue();
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div class="relative container mt-20 mx-auto p-6">
+    <SearchBar/>
+    <MovieList/>
+  </div>  
+</template>
+
+<script>
+// @ is an alias to /src
+import SearchBar from '@/components/SearchBar.vue'
+import MovieList from '@/components/MovieList.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    SearchBar,
+    MovieList
+  }
+}
+</script>
